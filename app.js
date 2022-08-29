272e04e9c092773e88a9ac7c7b6fecb7f871963d
lazy_coder <baitichhamza@gmail.com>
2022-08-29 11:35:36 +0100

    create basic math functionality

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+// add
+function add(a, b) {
+  return a + b
+}
+
+// subtract
+function subtract(a, b) {
+  return a - b
+}
+
+// multiply
+function multiply(a, b) {
+  return a * b
+}
+
+// devide
+function devide(a, b) {
+  return a / b
+}
+
+console.log(devide(5, 2))
