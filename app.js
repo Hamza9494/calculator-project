f96dba12a2f52c02100943aebc5df752bda52d8e
lazy_coder <baitichhamza@gmail.com>
2022-08-30 16:05:16 +0100

    fix varaible losing value bug

--- app.js
+++ app.js
@@ -19,10 +19,60 @@ function devide(a, b) {
 }
 
 // operate
-function operate(op, a, b) {
+function operate(a, op, b) {
   if (op == '+') return add(a, b)
   if (op == '-') return subtract(a, b)
   if (op == '/') return devide(a, b)
   if (op == '*') return multiply(a, b)
 }
-console.log(operate('/', 5, 3))
+
+// populate calculator
+const display = document.querySelector('.display')
+const btns = document.querySelectorAll('.num-btn')
+const operators = document.querySelectorAll('.operator')
+const equal = document.querySelector('.equal')
+
+let value1
+let operator
+let flag = false
+let value2
+
+function work() {
+  btns.forEach((num) => {
+    num.addEventListener('click', () => {
+      display.textContent = num.value
+      if (!flag) {
+        value1 = parseInt(num.value)
+        flag = true
+      }
+      if (flag) {
+        value2 = parseInt(num.value)
+      }
+    })
+  })
+  operators.forEach((op) => {
+    op.addEventListener('click', (e) => {
+      if (e.target.value === 'add') {
+        operator = '+'
+        flag = true
+      }
+      if (e.target.value === 'multiply') {
+        operator = '*'
+        flag = true
+      }
+      if (e.target.value === 'devide') {
+        operator = '/'
+        flag = true
+      }
+      if (e.target.value === 'subtract') {
+        operator = '-'
+        flag = true
+      }
+    })
+  })
+  equal.addEventListener('click', () => {
+    let result = operate(value1, operator, value2)
+    display.textContent = result
+  })
+}
+work()
